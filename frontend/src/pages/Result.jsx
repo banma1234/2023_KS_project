710266b4f882b5a79fe91bd6e35e5ffda1a61305
banma1234 <banma1234@gmail.com>
2023-03-30 15:49:03 +0900

    test: flask server의 api와 client간의 통신 테스트

--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 
 export default function Result() {
+    const [data, setData] = useState({
+        name: "",
+        date: "",
+        programming: "",
+    });
+
+    useEffect(async () => {
+        let response = await fetch("/api/data", {
+            method: "GET"
+        });
+
+        let data = await response.json();
+        setData({
+            name: data.Name,
+            date: data.date,
+            programming: data.programming,
+        });
+    }, []);
+
     return(
         <>
             <h1>Result</h1>
-            <h1>Result</h1>
-            <h1>Result</h1>
+            <br/><hr/>
+            <ul>
+                <li>name : {data.name}</li>
+                <li>date : {data.date}</li>
+                <li>programming : {data.programming}</li>
+            </ul>
         </>
     );
 };
