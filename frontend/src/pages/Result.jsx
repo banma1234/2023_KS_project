324cab7ed220e57454a82f4b320a942833019e40
banma1234 <banma1234@gmail.com>
2023-03-30 15:09:54 +0900

    feat: 기본 페이지 환경설정

---
+++ frontend/src/pages/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Result() {
+    return(
+        <>
+            <h1>Result</h1>
+            <h1>Result</h1>
+            <h1>Result</h1>
+        </>
+    );
+};
