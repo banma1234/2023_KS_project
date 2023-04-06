951a682956b0d056f3ccbc4ac45378b8de8c17fd
banma1234 <banma1234@gmail.com>
2023-04-06 18:35:36 +0900

    design: 전역 스타일 작성

---
+++ frontend/src/styles/globals.jsx
@@ -0,0 +1,11 @@
+export const Globals = {
+  palette: {
+    // 키 컬러
+    $color_purple: "#7f4dd1",
+    $color_green: "#9fd14d",
+
+    // 무채색
+    $color_gray: "#f7f6fb",
+    $color_black: "#262626",
+  },
+};
