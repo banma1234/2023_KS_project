ed22c4563f6551188e701dbed11b7ebb6b26b335
banma1234 <banma1234@gmail.com>
2023-04-06 18:35:00 +0900

    feat: about page 작성 및 라우팅

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,13 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
-import { Home, Result } from '../pages';
+import { Routes, Route } from "react-router-dom";
+import { Home, Result, About } from "../pages";
 
 const Router = () => {
-    return(
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/result" element={<Result/>} />
-        </Routes>
-    );
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/result" element={<Result />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+};
 
 export default Router;
